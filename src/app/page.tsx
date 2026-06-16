25f749c876a74ebd863f29ff660389c9004a9363
Your Name <your.email@example.com>
2026-06-16 22:10:25 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,82 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="portfolio-container">
+      
+		<section id="home" className="flex">
+			<div className="flex-1">	
+				<h1 className="font-bold uppercase text-[48px] leading-11 mx-5 mt-10">
+					Paginas Web profesionales para negocios
+				</h1>
+				<h2 className="text-[#c3c3c3] text-[20px] leading-tight mx-5 mt-2">
+					Portafolio de paginas web de clientes diseñadas para potenciar sus negocios
+				</h2>
+			</div>
+			<div className="flex-1 flex justify-center items-center">
+				<p> imagen de portada  </p>
+				{/* <img src="" alt="" /> */}
+				
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+			</div>
+		</section>
+		
+		<section id="inicio" className="flex flex-col justify-center items-center mt-10">
+			<button className="border-4 border-[#38bdf8] text-[#38bdf8] font-bold uppercase m-5 px-6 py-3 tracking-wider hover:bg-[#38bdf8] hover:text-[#0d0e12] hover:shadow-[0_0_15px_#38bdf8] transition-all duration-300 cursor-pointer">
+			Ver mis proyectos
+			</button>
+		</section>
+
+		<section id="portafolio">
+			<h1 className="font-bold uppercase text-[48px] leading-11 mx-5 mt-10">
+				Portafolio	
+			</h1>			
+			<h2 className="text-[#c3c3c3] text-[20px] leading-tight mx-5 mt-2">
+				Los proyectos destacados realizado para clientes.
+			</h2>
+		</section>	
+
+		<section id="portafolio-1" className="mt-10">
+			<h1 className="font-bold  text-[18px] leading-11 mx-5">
+				Restaurantes y Cafeterias
+			</h1>
+			<div className="grid grid-cols-2 justify-items-center p-10 gap-10">
+				<div className="bg-[#1e2027] border-2 border-[#2e323f] w-full h-108">
+
+				</div>
+				<div className="bg-[#1e2027] border-2 border-[#2e323f] w-full h-108">
+
+				</div>
+			</div>
+		</section>
+
+		
+		<section id="portafolio-2" className="mt-10">
+			<h1 className="font-bold  text-[18px] leading-11 mx-5">
+				Restaurantes y Cafeterias
+			</h1>
+			<div className="grid grid-cols-2 justify-items-center p-10 gap-10">
+				<div className="bg-[#1e2027] border-2 border-[#2e323f] w-full h-108">
+
+				</div>
+				<div className="bg-[#1e2027] border-2 border-[#2e323f] w-full h-108">
+
+				</div>
+			</div>
+		</section>
+
+		
+		<section id="portafolio-3" className="mt-10">
+			<h1 className="font-bold  text-[18px] leading-11 mx-5">
+				Restaurantes y Cafeterias
+			</h1>
+			<div className="grid grid-cols-2 justify-items-center p-10 gap-10">
+				<div className="bg-[#1e2027] border-2 border-[#2e323f] w-full h-108">
+
+				</div>
+				<div className="bg-[#1e2027] border-2 border-[#2e323f] w-full h-108">
+
+				</div>
+			</div>
+		</section>
     </div>
   );
 }
